c9ee36d72b96bfef53f017cb4a61650f1808540a
shreecheryl <shreecheryl@att.net>
2017-05-17 10:18:56 -0700

    in progress 051717

--- app/assets/js/cheryl.js
+++ app/assets/js/cheryl.js
@@ -1,22 +1,26 @@
-$(function () {
-    $("nav li:first a").addClass("active");
-    
-    $("#all").on("click", function (event) {
-        event.preventDefault();
-        $("nav li a").removeClass("active");
-        $(this).addClass("active");
-        $("img:visible").fadeOut(500, "linear", function () {
-            $("img").fadeIn(500, "linear");
+(function ($) {
+    $.fn.imageShuffle = function () {
+        return this.each(function () {
+            $("li:first a").addClass("active");
+
+            $("li:first a").on("click", function (event) {
+                event.preventDefault();
+                $("li a").removeClass("active");
+                $(this).addClass("active");
+                $("img:visible").fadeOut(500, "linear", function () {
+                    $("img").fadeIn(500, "linear");
+                });
+            });
+
+            $("li:not(:first) a").on("click", function () {
+                event.preventDefault();
+                var category = $(this).attr("id");
+                $("li a").removeClass("active");
+                $(this).addClass("active");
+                $("img:visible").fadeOut(500, "linear", function () { 
+                    $("." + category).fadeIn(500, "linear");
+                });
+            });
         });
-    });
-    
-    $("nav li:not(:first) a").on("click", function () {
-        event.preventDefault();
-        var category = $(this).attr("id");
-        $("nav li a").removeClass("active");
-        $(this).addClass("active");
-        $("img:visible").fadeOut(500, "linear", function () { 
-            $("." + category).fadeIn(500, "linear");
-        });
-    });
-}); // end of document ready
+    }
+})(jQuery);
---
+++ build/assets/js/cheryl.js
@@ -0,0 +1,26 @@
+(function ($) {
+    $.fn.imageShuffle = function () {
+        return this.each(function () {
+            $("nav li:first a").addClass("active");
+
+            $("nav li:first a").on("click", function (event) {
+                event.preventDefault();
+                $("nav li a").removeClass("active");
+                $(this).addClass("active");
+                $("img:visible").fadeOut(500, "linear", function () {
+                    $("img").fadeIn(500, "linear");
+                });
+            });
+
+            $("nav li:not(:first) a").on("click", function () {
+                event.preventDefault();
+                var category = $(this).attr("id");
+                $("nav li a").removeClass("active");
+                $(this).addClass("active");
+                $("img:visible").fadeOut(500, "linear", function () { 
+                    $("." + category).fadeIn(500, "linear");
+                });
+            });
+        });
+    }
+})(jQuery);
