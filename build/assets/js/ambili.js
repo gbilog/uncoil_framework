cc59073661a822ba60afb56071cf120a2a1dd056
ambilyrenjith <ambilyrenjith@yahoo.com>
2017-05-25 21:26:35 -0700

    Style

--- build/assets/js/ambili.js
+++ build/assets/js/ambili.js
@@ -1,54 +1,38 @@
-/*
-$(function(){
-    
-    // 1. Set tab header (ul) css classes
-    var tab1 = $("li[data-tab=tab-1]");
-    tab1.addClass('tab-link current');
-    
-    var tab2 = $("li[data-tab=tab-2]");
-    tab2.addClass('tab-link');
-    
-    var tab3 = $("li[data-tab=tab-3]");
-    tab3.addClass('tab-link');
-    
-    var tab4 = $("li[data-tab=tab-4]");
-    tab4.addClass('tab-link');
-    
-    alert("Document is ready");
-    
-    // 2. Set tab content (div) css classes
-    $('#tab-1').addClass('tab-content current');
-    $('#tab-2').addClass('tab-content');
-    $('#tab-3').addClass('tab-content');
-    $('#tab-4').addClass('tab-content');
-    
-   //  3. Attach click events for tab headers
-    $('ul.tabs li').click(function(){
-        
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	});
-
-    
-});
-
-
-*/
 
 (function($) {
     
-    $.fn.tabs = function() {
-        return this.each(function() {
-          $("li[data-tab=tab-1]").addClass('tab-link current');
-          $("li[data-tab=tab-2]").addClass('tab-link');
-          $("li[data-tab=tab-3]").addClass('tab-link');
-          $("li[data-tab=tab-4]").addClass('tab-link');
-          console.log("Document is ready");    
-    });
-    
+    $.fn.Tabs = function() {
+        
+        $(this).addClass("tabs");
+        
+        $(this).find("li").each(function(i, ctrl){
+        
+            // 1. Set tab header (ul) css classes
+            // 2. Set tab content (div) css classes
+            
+            var x = $(ctrl).attr('data-tab');
+
+            if(i == 0) {
+                $("#" + x).addClass('tab-content current');
+                $(ctrl).addClass("tab-link current"); 
+                
+            } else {
+                $("#" + x).addClass('tab-content');
+                $(ctrl).addClass("tab-link");  
+            }
+        });
+        
+       //  3. Attach click events for tab headers
+        
+        $(this).find('li').click(function(){
+
+            var tab_id = $(this).attr('data-tab');
+
+            $('li.tab-link').removeClass('current');
+            $('.tab-content').removeClass('current');
+
+            $(this).addClass('current');
+            $("#"+tab_id).addClass('current');
+        });   
+    }
 })(jQuery);
