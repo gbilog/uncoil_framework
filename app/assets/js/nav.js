4969100c2f58e388b7d549755db587947b7b0756
Vlad Pasculescu <vpasculescu@Vlads-MacBook-Pro.local>
2017-06-01 01:07:28 -0700

    Side Menu

--- app/assets/js/nav.js
+++ app/assets/js/nav.js
@@ -1,8 +1,7 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
